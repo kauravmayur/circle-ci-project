09a8083505c73affeb607cee6d90bed59c302503
kauravmayur@gmail.com <kauravmayur@gmail.com>
2020-09-30 12:17:58 +0530

    update install.sh

--- build/install.sh
+++ build/install.sh
@@ -1,3 +1,7 @@
 #Install dependencies, SFDX CLI in this case
 echo "Installing Dependencies... "
 sudo npm install -global sfdx-cli
+sfdx update
+sfdx --version
+sfdx plugins:install salesforcedx@49.2.3
+sfdx plugins --core
